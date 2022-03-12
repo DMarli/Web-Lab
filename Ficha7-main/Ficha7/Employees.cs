d8410782a34427595422cc039868470f9cdfc1eb
DMarli <nobregamarli@gmail.com>
2022-03-12 15:29:39 +0000

    3

---
+++ Ficha7-main/Ficha7/Employees.cs
@@ -0,0 +1,13 @@
+﻿namespace Ficha7
+{
+    public class Employees
+    {
+        public Employees()
+        {
+           EmployeesList = new List<Employee>(); 
+        }
+        public List<Employee> EmployeesList { get; set; } //se este estiver no json diferente, não carrega. Json tem de estar EmployeesList
+        //lista não está inicializada, está null
+    }
+
+}
