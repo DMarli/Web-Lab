0b5e20d5b866a3af65b815d412ee4b3a3c564828
Marli <94609980+DMarli@users.noreply.github.com>
2022-03-15 13:01:59 +0000

    Add files via upload

---
+++ Ficha10/Models/Characters.cs
@@ -0,0 +1,18 @@
+﻿namespace Ficha10
+{
+    public class Characters : ICharacters
+    {
+        private List<Character> charactersList;
+
+        List<Character> ICharacters.CharactersList
+        {
+            get => charactersList;
+            set => charactersList = value;
+        }
+
+        public Characters()
+        {
+            charactersList = JsonLoader.LoadCharactersJSON();   
+        }
+    }
+}
---
+++ Ficha10/Models/Employees.cs
@@ -0,0 +1,18 @@
+﻿namespace Ficha10
+{
+    public class Employees: IEmployees
+    {
+        private List<Employee> employeesList;
+        List<Employee> IEmployees.EmployeesList 
+        { 
+          get => employeesList;
+          set => employeesList = value;
+        }
+
+        public Employees()
+        {
+            employeesList = JsonLoader.LoadEmployeesJSON();
+        }
+    }
+
+}
