11524ff9d39ca6ae83616473f3984d437d53bac5
DMarli <94609980+DMarli@users.noreply.github.com>
2022-03-15 13:04:05 +0000

    1

---
+++ Ficha10.3/Models/Characters.cs
@@ -0,0 +1,7 @@
+﻿namespace Ficha10._3.Models
+{
+    public class Characters
+    {
+       public List <Character> CharactersList { get; set; }
+    }
+}
---
+++ Ficha10.3/Models/Employees.cs
@@ -0,0 +1,8 @@
+﻿namespace Ficha10._3.Models
+{
+    public class Employees
+    {
+        public List<Employee> EmployeesList { get; set; }
+    }
+
+}
