69168fd5510b3ddbc4a7b1b5b4e764dd2fbaa8ee
DMarli <94609980+DMarli@users.noreply.github.com>
2022-03-24 11:33:33 +0000

    1

---
+++ Ficha14/Ficha14/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Ficha14.Models;
+using Ficha14.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Ficha14.Controllers
+{
+
+    public class HomeController : Controller
+    {
+        private readonly IConfiguration config;
+        private readonly IJWTService tokenService;
+        private readonly IUserService userService;        
+
+        public HomeController(IConfiguration config, IJWTService tokenService, IUserService userService)
+        {
+            this.config = config;
+            this.tokenService = tokenService;
+            this.userService = userService;
+        }     
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+     
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
