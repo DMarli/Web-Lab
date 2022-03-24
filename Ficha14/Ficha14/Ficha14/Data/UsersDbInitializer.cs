f28f59f3e341d0b811434616b5fa32220428ef34
DMarli <nobregamarli@gmail.com>
2022-03-24 15:41:35 +0000

    Com Login

---
+++ Ficha14/Ficha14/Ficha14/Data/UsersDbInitializer.cs
@@ -0,0 +1,23 @@
+﻿using Ficha14.Models;
+
+namespace Ficha14.Data
+{
+    public static class UsersDbInitializer
+    {
+        public static void InsertData(UserContext context)
+        {          
+            // Adds some users
+            context.Users.Add(new User //model
+            {
+               
+                UserName = "djardim",              
+                Password = "qwerty",
+                Role = "manager",
+                Email = "djardim@gmail.com"
+            });
+            
+            // Saves changes
+            context.SaveChanges();
+        }
+    }
+}
